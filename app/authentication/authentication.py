52d55c738e72c0122e71f7ad9fa9aaf1efe61ec0
AishwaryadeviPerumal <aish.cse.gce@gmail.com>
2025-11-29 08:41:05 +0800

    smart investment agent

--- app/authentication/authentication.py
+++ app/authentication/authentication.py
@@ -3,9 +3,8 @@ import sys
 print(sys.path)
 from jproperties import Properties
 
-
 props = Properties()
-with open('../app-config.properties', 'rb') as props_file:
+with open('../properties/app-config.properties', 'rb') as props_file:
     props.load(props_file)
 google_api_key = props.get('GOOGLE_API_KEY').data
 os.environ["GOOGLE_API_KEY"] = google_api_key
